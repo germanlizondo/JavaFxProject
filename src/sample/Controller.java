64fe2f6702e35e0f08c2d4820ef1e98be537b577
germanlizondo <german9lp@gmail.com>
2019-03-11 13:35:09 +0100

    version 0.1

---
+++ src/sample/Controller.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.fxml.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+
+public class Controller {
+
+
+
+    public void canviarEscenaAction(javafx.event.ActionEvent actionEvent) {
+
+
+
+
+
+
+
+    }
+}
