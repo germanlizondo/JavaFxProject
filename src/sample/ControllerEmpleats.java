a9d277a85104d735e0e82d7799029b73d1245d89
german <german9lp@gmail.com>
2019-03-19 20:35:01 +0100

    table 0.5

--- src/sample/ControllerEmpleats.java
+++ src/sample/ControllerEmpleats.java
@@ -1,17 +1,28 @@
 package sample;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+
 
 public class ControllerEmpleats {
 
     DomXml domXml = new DomXml();
+    @FXML
+    private Button home;
+
+    @FXML
+    private Button empleats;
+
+    @FXML
+    private Button ventes;
 
     @FXML
     private TextField nom;
@@ -25,10 +36,13 @@ public class ControllerEmpleats {
     @FXML
     private TextField ss;
 
+    @FXML
+    private TableView tabla;
 
     @FXML
     public void initialize() {
         System.out.println("second");
+
     }
 
     @FXML
@@ -41,5 +55,45 @@ public class ControllerEmpleats {
         cognom.setText("");
         edad.setText("");
         ss.setText("");
+    }
+
+
+    @FXML
+    public void canviarEscenaAction(ActionEvent actionEvent) {
+
+        Stage stage = null;
+        Parent root = null;
+        try {
+
+            if(actionEvent.getSource()==home){
+                System.out.println("Home");
+
+                stage=(Stage) empleats.getScene().getWindow();
+                root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+
+            }else if(actionEvent.getSource()==empleats){
+                System.out.println("Empleats");
+
+
+
+            }else if(actionEvent.getSource()==ventes){
+                System.out.println("Ventes");
+
+                stage=(Stage) empleats.getScene().getWindow();
+                root = FXMLLoader.load(getClass().getResource("ventes.fxml"));
+
+            }
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        }catch (Exception e){
+            e.getStackTrace();
+        }
+
+
+
     }
 }
--- src/sample/VentesController.java
+++ src/sample/VentesController.java
@@ -1,9 +1,63 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 import java.util.Stack;
 
 public class VentesController {
 
+    @FXML
+    private Button home;
+
+    @FXML
+    private Button empleats;
+
+    @FXML
+    private Button ventes;
 
 
+    @FXML
+    public void canviarEscenaAction(ActionEvent actionEvent) {
+
+        Stage stage = null;
+        Parent root = null;
+        try {
+
+            if(actionEvent.getSource()==home){
+                System.out.println("Home");
+
+                stage=(Stage) empleats.getScene().getWindow();
+                root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+
+            }else if(actionEvent.getSource()==empleats){
+                System.out.println("Empleats");
+
+                stage=(Stage) empleats.getScene().getWindow();
+                root = FXMLLoader.load(getClass().getResource("empleats.fxml"));
+
+            }else if(actionEvent.getSource()==ventes){
+                System.out.println("Ventes");
+
+
+
+            }
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        }catch (Exception e){
+            e.getStackTrace();
+        }
+
+
+
+    }
 }
